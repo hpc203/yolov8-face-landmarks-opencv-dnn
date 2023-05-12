3deb14bb4454527fe81e94383c771078efc7c465
hpc203 <1749069040@qq.com>
2023-05-12 15:00:33 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,228 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+
+
+class YOLOv8_face
+{
+public:
+	YOLOv8_face(string modelpath, float confThreshold, float nmsThreshold);
+	void detect(Mat& frame);
+private:
+	Mat resize_image(Mat srcimg, int *newh, int *neww, int *padh, int *padw);
+	const bool keep_ratio = true;
+	const int inpWidth = 640;
+	const int inpHeight = 640;
+	float confThreshold;
+	float nmsThreshold;
+	const int num_class = 1;  ///只有人脸这一个类别
+	const int reg_max = 16;
+	Net net;
+	void softmax_(const float* x, float* y, int length);
+	void generate_proposal(Mat out, vector<Rect>& boxes, vector<float>& confidences, vector< vector<Point>>& landmarks, int imgh, int imgw, float ratioh, float ratiow, int padh, int padw);
+	void drawPred(float conf, int left, int top, int right, int bottom, Mat& frame, vector<Point> landmark);
+};
+
+static inline float sigmoid_x(float x)
+{
+	return static_cast<float>(1.f / (1.f + exp(-x)));
+}
+
+YOLOv8_face::YOLOv8_face(string modelpath, float confThreshold, float nmsThreshold)
+{
+	this->confThreshold = confThreshold;
+	this->nmsThreshold = nmsThreshold;
+	this->net = readNet(modelpath);
+}
+
+Mat YOLOv8_face::resize_image(Mat srcimg, int *newh, int *neww, int *padh, int *padw)
+{
+	int srch = srcimg.rows, srcw = srcimg.cols;
+	*newh = this->inpHeight;
+	*neww = this->inpWidth;
+	Mat dstimg;
+	if (this->keep_ratio && srch != srcw) {
+		float hw_scale = (float)srch / srcw;
+		if (hw_scale > 1) {
+			*newh = this->inpHeight;
+			*neww = int(this->inpWidth / hw_scale);
+			resize(srcimg, dstimg, Size(*neww, *newh), INTER_AREA);
+			*padw = int((this->inpWidth - *neww) * 0.5);
+			copyMakeBorder(dstimg, dstimg, 0, 0, *padw, this->inpWidth - *neww - *padw, BORDER_CONSTANT, 0);
+		}
+		else {
+			*newh = (int)this->inpHeight * hw_scale;
+			*neww = this->inpWidth;
+			resize(srcimg, dstimg, Size(*neww, *newh), INTER_AREA);
+			*padh = (int)(this->inpHeight - *newh) * 0.5;
+			copyMakeBorder(dstimg, dstimg, *padh, this->inpHeight - *newh - *padh, 0, 0, BORDER_CONSTANT, 0);
+		}
+	}
+	else {
+		resize(srcimg, dstimg, Size(*neww, *newh), INTER_AREA);
+	}
+	return dstimg;
+}
+
+void YOLOv8_face::drawPred(float conf, int left, int top, int right, int bottom, Mat& frame, vector<Point> landmark)   // Draw the predicted bounding box
+{
+	//Draw a rectangle displaying the bounding box
+	rectangle(frame, Point(left, top), Point(right, bottom), Scalar(0, 0, 255), 3);
+
+	//Get the label for the class name and its confidence
+	string label = format("face:%.2f", conf);
+
+	//Display the label at the top of the bounding box
+	/*int baseLine;
+	Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+	top = max(top, labelSize.height);
+	rectangle(frame, Point(left, top - int(1.5 * labelSize.height)), Point(left + int(1.5 * labelSize.width), top + baseLine), Scalar(0, 255, 0), FILLED);*/
+	putText(frame, label, Point(left, top-5), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+	for (int i = 0; i < 5; i++)
+	{
+		circle(frame, landmark[i], 4, Scalar(0, 255, 0), -1);
+	}
+}
+
+void YOLOv8_face::softmax_(const float* x, float* y, int length)
+{
+	float sum = 0;
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		y[i] = exp(x[i]);
+		sum += y[i];
+	}
+	for (i = 0; i < length; i++)
+	{
+		y[i] /= sum;
+	}
+}
+
+void YOLOv8_face::generate_proposal(Mat out, vector<Rect>& boxes, vector<float>& confidences, vector< vector<Point>>& landmarks, int imgh,int imgw, float ratioh, float ratiow, int padh, int padw)
+{
+	const int feat_h = out.size[2];
+	const int feat_w = out.size[3];
+	cout << out.size[1] << "," << out.size[2] << "," << out.size[3] << endl;
+	const int stride = (int)ceil((float)inpHeight / feat_h);
+	const int area = feat_h * feat_w;
+	float* ptr = (float*)out.data;
+	float* ptr_cls = ptr + area * reg_max * 4;
+	float* ptr_kp = ptr + area * (reg_max * 4 + num_class);
+
+	for (int i = 0; i < feat_h; i++)
+	{
+		for (int j = 0; j < feat_w; j++)
+		{
+			const int index = i * feat_w + j;
+			int cls_id = -1;
+			float max_conf = -10000;
+			for (int k = 0; k < num_class; k++)
+			{
+				float conf = ptr_cls[k*area + index];
+				if (conf > max_conf)
+				{
+					max_conf = conf;
+					cls_id = k;
+				}
+			}
+			float box_prob = sigmoid_x(max_conf);
+			if (box_prob > this->confThreshold)
+			{
+				float pred_ltrb[4];
+				float* dfl_value = new float[reg_max];
+				float* dfl_softmax = new float[reg_max];
+				for (int k = 0; k < 4; k++)
+				{
+					for (int n = 0; n < reg_max; n++)
+					{
+						dfl_value[n] = ptr[(k*reg_max + n)*area + index];
+					}
+					softmax_(dfl_value, dfl_softmax, reg_max);
+
+					float dis = 0.f;
+					for (int n = 0; n < reg_max; n++)
+					{
+						dis += n * dfl_softmax[n];
+					}
+
+					pred_ltrb[k] = dis * stride;
+				}
+				float cx = (j + 0.5f)*stride;
+				float cy = (i + 0.5f)*stride;
+				float xmin = max((cx - pred_ltrb[0] - padw)*ratiow, 0.f);  ///还原回到原图
+				float ymin = max((cy - pred_ltrb[1] - padh)*ratioh, 0.f);
+				float xmax = min((cx + pred_ltrb[2] - padw)*ratiow, float(imgw - 1));
+				float ymax = min((cy + pred_ltrb[3] - padh)*ratioh, float(imgh - 1));
+				Rect box = Rect(int(xmin), int(ymin), int(xmax - xmin), int(ymax - ymin));
+				boxes.push_back(box);
+				confidences.push_back(box_prob);
+
+				vector<Point> kpts(5);
+				for (int k = 0; k < 5; k++)
+				{
+					float x = ((ptr_kp[(k * 3)*area + index] * 2 + j)*stride - padw)*ratiow;  ///还原回到原图
+					float y = ((ptr_kp[(k * 3 + 1)*area + index] * 2 + i)*stride - padh)*ratioh;
+					///float pt_conf = ptr_kp[(k * 3 + 2)*area + index];
+					kpts[k] = Point(int(x), int(y));
+				}
+				landmarks.push_back(kpts);
+			}
+		}
+	}
+}
+
+
+void YOLOv8_face::detect(Mat& srcimg)
+{
+	int newh = 0, neww = 0, padh = 0, padw = 0;
+	Mat dst = this->resize_image(srcimg, &newh, &neww, &padh, &padw);
+	Mat blob;
+	blobFromImage(dst, blob, 1 / 255.0, Size(this->inpWidth, this->inpHeight), Scalar(0, 0, 0), true, false);
+	this->net.setInput(blob);
+	vector<Mat> outs;
+	this->net.forward(outs, this->net.getUnconnectedOutLayersNames());
+
+	/////generate proposals
+	vector<Rect> boxes;
+	vector<float> confidences;
+	vector< vector<Point>> landmarks;
+	float ratioh = (float)srcimg.rows / newh, ratiow = (float)srcimg.cols / neww;
+
+	generate_proposal(outs[0], boxes, confidences, landmarks, srcimg.rows, srcimg.cols, ratioh, ratiow, padh, padw);
+	generate_proposal(outs[1], boxes, confidences, landmarks, srcimg.rows, srcimg.cols, ratioh, ratiow, padh, padw);
+	generate_proposal(outs[2], boxes, confidences, landmarks, srcimg.rows, srcimg.cols, ratioh, ratiow, padh, padw);
+
+	// Perform non maximum suppression to eliminate redundant overlapping boxes with
+	// lower confidences
+	vector<int> indices;
+	NMSBoxes(boxes, confidences, this->confThreshold, this->nmsThreshold, indices);
+	for (size_t i = 0; i < indices.size(); ++i)
+	{
+		int idx = indices[i];
+		Rect box = boxes[idx];
+		this->drawPred(confidences[idx], box.x, box.y,
+			box.x + box.width, box.y + box.height, srcimg, landmarks[idx]);
+	}
+}
+
+int main()
+{
+	YOLOv8_face YOLOv8_face_model("weights/yolov8n-face.onnx", 0.45, 0.5);
+	string imgpath = "images/2.jpg";
+	Mat srcimg = imread(imgpath);
+	YOLOv8_face_model.detect(srcimg);
+	
+	static const string kWinName = "Deep learning face detection use OpenCV";
+	namedWindow(kWinName, WINDOW_NORMAL);
+	imshow(kWinName, srcimg);
+	waitKey(0);
+	destroyAllWindows();
+}
---
+++ main.py
@@ -0,0 +1,178 @@
+import cv2
+import numpy as np
+import math
+import argparse
+
+class YOLOv8_face:
+    def __init__(self, path, conf_thres=0.2, iou_thres=0.5):
+        self.conf_threshold = conf_thres
+        self.iou_threshold = iou_thres
+        self.class_names = ['face']
+        self.num_classes = len(self.class_names)
+        # Initialize model
+        self.net = cv2.dnn.readNet(path)
+        self.input_height = 640
+        self.input_width = 640
+        self.reg_max = 16
+
+        self.project = np.arange(self.reg_max)
+        self.strides = (8, 16, 32)
+        self.feats_hw = [(math.ceil(self.input_height / self.strides[i]), math.ceil(self.input_width / self.strides[i])) for i in range(len(self.strides))]
+        self.anchors = self.make_anchors(self.feats_hw)
+
+    def make_anchors(self, feats_hw, grid_cell_offset=0.5):
+        """Generate anchors from features."""
+        anchor_points = []
+        for i, stride in enumerate(self.strides):
+            h,w = feats_hw[i]
+            x = np.arange(0, w) + grid_cell_offset  # shift x
+            y = np.arange(0, h) + grid_cell_offset  # shift y
+            sx, sy = np.meshgrid(x, y)
+            # sy, sx = np.meshgrid(y, x)
+            anchor_points.append(np.stack((sx, sy), axis=-1).reshape(-1, 2))
+        return anchor_points
+
+    def softmax(self, x, axis=1):
+        x_exp = np.exp(x)
+        # 如果是列向量，则axis=0
+        x_sum = np.sum(x_exp, axis=axis, keepdims=True)
+        s = x_exp / x_sum
+        return s
+    
+    def resize_image(self, srcimg, keep_ratio=True):
+        top, left, newh, neww = 0, 0, self.input_width, self.input_height
+        if keep_ratio and srcimg.shape[0] != srcimg.shape[1]:
+            hw_scale = srcimg.shape[0] / srcimg.shape[1]
+            if hw_scale > 1:
+                newh, neww = self.input_height, int(self.input_width / hw_scale)
+                img = cv2.resize(srcimg, (neww, newh), interpolation=cv2.INTER_AREA)
+                left = int((self.input_width - neww) * 0.5)
+                img = cv2.copyMakeBorder(img, 0, 0, left, self.input_width - neww - left, cv2.BORDER_CONSTANT,
+                                         value=(0, 0, 0))  # add border
+            else:
+                newh, neww = int(self.input_height * hw_scale), self.input_width
+                img = cv2.resize(srcimg, (neww, newh), interpolation=cv2.INTER_AREA)
+                top = int((self.input_height - newh) * 0.5)
+                img = cv2.copyMakeBorder(img, top, self.input_height - newh - top, 0, 0, cv2.BORDER_CONSTANT,
+                                         value=(0, 0, 0))
+        else:
+            img = cv2.resize(srcimg, (self.input_width, self.input_height), interpolation=cv2.INTER_AREA)
+        return img, newh, neww, top, left
+
+    def detect(self, srcimg):
+        input_img, newh, neww, padh, padw = self.resize_image(cv2.cvtColor(srcimg, cv2.COLOR_BGR2RGB))
+        scale_h, scale_w = srcimg.shape[0]/newh, srcimg.shape[1]/neww
+        input_img = input_img.astype(np.float32) / 255.0
+
+        blob = cv2.dnn.blobFromImage(input_img)
+        self.net.setInput(blob)
+        outputs = self.net.forward(self.net.getUnconnectedOutLayersNames())
+        if float(cv2.__version__[:3])>=4.7:
+            outputs = [outputs[2], outputs[0], outputs[1]] ###opencv4.7需要这一步，opencv4.5不需要
+        # Perform inference on the image
+        det_bboxes, det_conf, det_classid, landmarks = self.post_process(outputs, scale_h, scale_w, padh, padw)
+        return det_bboxes, det_conf, det_classid, landmarks
+
+    def post_process(self, preds, scale_h, scale_w, padh, padw):
+        bboxes, scores, landmarks = [], [], []
+        for i, (stride, anchors) in enumerate(zip(self.strides, self.anchors)):
+            preds[i] = preds[i].transpose((0, 2, 3, 1))
+            
+            box = preds[i][..., :self.reg_max * 4]
+            cls = 1 / (1 + np.exp(-preds[i][..., self.reg_max * 4:-15])).reshape((-1,1))
+            kpts = preds[i][..., -15:].reshape((-1,15)) ### x1,y1,score1, ..., x5,y5,score5
+
+            # tmp = box.reshape(self.feats_hw[i][0], self.feats_hw[i][1], 4, self.reg_max)
+            tmp = box.reshape(-1, 4, self.reg_max)
+            bbox_pred = self.softmax(tmp, axis=-1)
+            bbox_pred = np.dot(bbox_pred, self.project).reshape((-1,4))
+
+            bbox = self.distance2bbox(anchors, bbox_pred, max_shape=(self.input_height, self.input_width)) * stride
+            kpts[:, 0::3] = (kpts[:, 0::3] * 2.0 + (anchors[:, 0].reshape((-1,1)) - 0.5)) * stride
+            kpts[:, 1::3] = (kpts[:, 1::3] * 2.0 + (anchors[:, 1].reshape((-1,1)) - 0.5)) * stride
+            kpts[:, 2::3] = 1 / (1+np.exp(-kpts[:, 2::3]))
+
+            bbox -= np.array([[padw, padh, padw, padh]])  ###合理使用广播法则
+            bbox *= np.array([[scale_w, scale_h, scale_w, scale_h]])
+            kpts -= np.tile(np.array([padw, padh, 0]), 5).reshape((1,15))
+            kpts *= np.tile(np.array([scale_w, scale_h, 1]), 5).reshape((1,15))
+
+            bboxes.append(bbox)
+            scores.append(cls)
+            landmarks.append(kpts)
+
+        bboxes = np.concatenate(bboxes, axis=0)
+        scores = np.concatenate(scores, axis=0)
+        landmarks = np.concatenate(landmarks, axis=0)
+    
+        bboxes_wh = bboxes.copy()
+        bboxes_wh[:, 2:4] = bboxes[:, 2:4] - bboxes[:, 0:2]  ####xywh
+        classIds = np.argmax(scores, axis=1)
+        confidences = np.max(scores, axis=1)  ####max_class_confidence
+        
+        mask = confidences>self.conf_threshold
+        bboxes_wh = bboxes_wh[mask]  ###合理使用广播法则
+        confidences = confidences[mask]
+        classIds = classIds[mask]
+        landmarks = landmarks[mask]
+        
+        indices = cv2.dnn.NMSBoxes(bboxes_wh.tolist(), confidences.tolist(), self.conf_threshold,
+                                   self.iou_threshold).flatten()
+        if len(indices) > 0:
+            mlvl_bboxes = bboxes_wh[indices]
+            confidences = confidences[indices]
+            classIds = classIds[indices]
+            landmarks = landmarks[indices]
+            return mlvl_bboxes, confidences, classIds, landmarks
+        else:
+            print('nothing detect')
+            return np.array([]), np.array([]), np.array([]), np.array([])
+
+    def distance2bbox(self, points, distance, max_shape=None):
+        x1 = points[:, 0] - distance[:, 0]
+        y1 = points[:, 1] - distance[:, 1]
+        x2 = points[:, 0] + distance[:, 2]
+        y2 = points[:, 1] + distance[:, 3]
+        if max_shape is not None:
+            x1 = np.clip(x1, 0, max_shape[1])
+            y1 = np.clip(y1, 0, max_shape[0])
+            x2 = np.clip(x2, 0, max_shape[1])
+            y2 = np.clip(y2, 0, max_shape[0])
+        return np.stack([x1, y1, x2, y2], axis=-1)
+    
+    def draw_detections(self, image, boxes, scores, kpts):
+        for box, score, kp in zip(boxes, scores, kpts):
+            x, y, w, h = box.astype(int)
+            # Draw rectangle
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+            cv2.putText(image, "face:"+str(round(score,2)), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
+            for i in range(5):
+                cv2.circle(image, (int(kp[i * 3]), int(kp[i * 3 + 1])), 4, (0, 255, 0), thickness=-1)
+                # cv2.putText(image, str(i), (int(kp[i * 3]), int(kp[i * 3 + 1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), thickness=1)
+        return image
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--imgpath', type=str, default='images/2.jpg', help="image path")
+    parser.add_argument('--modelpath', type=str, default='weights/yolov8n-face.onnx',
+                        help="onnx filepath")
+    parser.add_argument('--confThreshold', default=0.45, type=float, help='class confidence')
+    parser.add_argument('--nmsThreshold', default=0.5, type=float, help='nms iou thresh')
+    args = parser.parse_args()
+
+    # Initialize YOLOv8_face object detector
+    YOLOv8_face_detector = YOLOv8_face(args.modelpath, conf_thres=args.confThreshold, iou_thres=args.nmsThreshold)
+    srcimg = cv2.imread(args.imgpath)
+
+    # Detect Objects
+    boxes, scores, classids, kpts = YOLOv8_face_detector.detect(srcimg)
+
+    # Draw detections
+    dstimg = YOLOv8_face_detector.draw_detections(srcimg, boxes, scores, kpts)
+    #cv2.imwrite('result.jpg', dstimg)
+    winName = 'Deep learning face detection use OpenCV'
+    cv2.namedWindow(winName, 0)
+    cv2.imshow(winName, dstimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
